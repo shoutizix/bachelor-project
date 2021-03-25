64fed7e1f6a409dd6f8ebd01f944e2054c7d2eb1
Noah Kaltenrieder <noah.kaltenrieder@epfl.ch>
2021-03-25 13:54:00 +0100

    fixes orientation of data and convolve

--- detectsat/extract_psf.py
+++ detectsat/extract_psf.py
@@ -1,11 +1,24 @@
 import astropy.io.fits as fits
 import utils.prologue as prologue
+from scipy import ndimage
+from matplotlib import pyplot as plt
+import numpy as np
+from utils.mosaic import *
+import imageio as imgio
 
 def main(args):
-    x_star,y_star = 3274,7977
-    radius = 10
+    #x_star,y_star = 3274,7977
+    #radius = 10
+    x_star,y_star = 3069,7821
+    radius = 15
+    #x_star,y_star = 2585,7765
+    #radius = 5
     data = fits.open(args.i)[0].data
-    star = data[y_star-radius:y_star+radius, x_star-radius:x_star+radius]
+
+    scaled_data = scale_image(data[::-1].copy())
+    star = scaled_data[y_star-radius:y_star+radius, x_star-radius:x_star+radius]
+    data_convolved = ndimage.convolve(scaled_data, star, mode='constant', cval=0.0)
+    imgio.imwrite('convolved.png', data_convolved)
 
 if __name__ == '__main__':
     main(prologue.get_args())
